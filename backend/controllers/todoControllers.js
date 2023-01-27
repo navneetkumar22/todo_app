35d37ae1eb892c07c1701064d92fc73284337a63
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-01-28 00:31:21 +0530

    controllers update

--- backend/controllers/todoControllers.js
+++ backend/controllers/todoControllers.js
@@ -5,12 +5,10 @@ exports.home = (req, res) => {
     res.send("<h1>Hello world! This is the todo app</h1>");
 }
 
-//create a Todo route
+//create a Todo
 exports.createTodo = async (req, res) => {
     try {
         const { title } = req.body;
-        // const tasks = [];
-
 
         //validate
         if (!title) {
@@ -49,6 +47,7 @@ exports.editTodo = async (req, res) => {
         res.status(200).json({
             success: true,
             message: "Todo updateded successfully",
+            todo
         })
     } catch (error) {
         console.log(error);
@@ -68,27 +67,77 @@ exports.deleteTodo = async (req, res) => {
     }
 }
 
+
+
 /**
  *  Adding a Task
+ *  Editing a Task
  *  Deleting a task
  */
 
-// Add a task in the existing todo
+
+// Add a task in the existing Todo
 exports.addTask = async (req, res) => {
     try {
 
         const todoExists = await Todo.findById(req.params.id);
+
+        //validation
         if (!todoExists) {
             throw new Error("Todo does not exist");
         }
 
         todoExists.tasks.push({ taskTitle: req.body.task });
-
         const todo = await Todo.findByIdAndUpdate(req.params.id, todoExists);
+
         res.status(200).json({
             success: true,
             message: "Tasks added successfully",
             todo
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//Edit a Task in the existing Todo
+exports.editTask = async (req, res) => {
+    try {
+
+        const { todoId, taskId } = req.params;
+        const todoExist = await Todo.findById(todoId);
+
+        //todo validation - check if todo exist
+        if (!todoExist) {
+            throw new Error("Todo does not exists")
+        }
+
+        //task validation - check if task exist
+        const taskExist = todoExist.tasks.some(element => element._id == taskId);
+        // returns true if task found
+        if (!taskExist) {
+            throw new Error("Task does not exists")
+        }
+
+        //editing the task
+        const newTask = todoExist.tasks.map((element) => {
+            if (element.id === taskId) {
+                element.taskTitle = req.body.task;
+            } else {
+                // throw new Error("Task does not even exist")
+                return element;
+            }
+            console.log(element);
+        })
+
+        //Updating existing Todo with the new Task
+        todoExist.tasks = newTask;
+        const updatedTodo = await Todo.findByIdAndUpdate(todoId, todoExist);
+        res.status(200).json({
+            success: true,
+            message: "Task is updated successfully",
+            todoExist
         })
 
     } catch (error) {
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { home, createTodo, getAllTodos, editTodo, deleteTodo, addTask } = require("../controllers/todoControllers");
+const { home, createTodo, getAllTodos, editTodo, deleteTodo, addTask, editTask } = require("../controllers/todoControllers");
 const router = express.Router();
 
 router.get("/", home);
@@ -9,6 +9,7 @@ router.put("/editTodo/:id", editTodo);
 router.delete("/deleteTodo/:id", deleteTodo);
 
 //tasks
-router.put(`/addTask/:id`, addTask);
+router.put("/addTask/:id", addTask);
+router.put("/editTask/:todoId/:taskId", editTask);
 
 module.exports = router;
